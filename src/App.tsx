d0990afb38b3bfa4378fa8598df73d2a579b84ff
Luuk Ros <rosluuk@gmail.com>
2023-09-12 11:45:40 +0200

    additional button variants;

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,53 @@ function App() {
           <Button size="lg">Large</Button>
         </div>
       </section>
+
+      {/* Shapes */}
+      <section className="flex items-center gap-4">
+        <h2>Shapes</h2>
+        <div className="flex items-center justify-start gap-4">
+          <Button>Rounded</Button>
+          <Button shape="pill">Pill button</Button>
+          <Button shape="square">Square button</Button>
+        </div>
+      </section>
+
+      {/* Tones */}
+      <section className="flex items-center gap-4">
+        <h2>Tones & impact</h2>
+        <div className="flex flex-col gap-4">
+          <Button>Default</Button>
+          <Button impact="light">Default</Button>
+          <Button impact="bordered">Default</Button>
+        </div>
+        <div className="flex flex-col gap-4">
+          <Button tone="success">Success</Button>
+          <Button tone="success" impact="light">
+            Success
+          </Button>
+          <Button tone="success" impact="bordered">
+            Success
+          </Button>
+        </div>
+        <div className="flex flex-col gap-4">
+          <Button tone="warning">Warning</Button>
+          <Button tone="warning" impact="light">
+            Warning
+          </Button>
+          <Button tone="warning" impact="bordered">
+            Success
+          </Button>
+        </div>
+        <div className="flex flex-col gap-4">
+          <Button tone="danger">Danger</Button>
+          <Button tone="danger" impact="light">
+            Danger
+          </Button>
+          <Button tone="danger" impact="bordered">
+            Success
+          </Button>
+        </div>
+      </section>
     </main>
   );
 }
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -7,6 +7,49 @@ type ButtonProps = {
   size?: "sm" | "md" | "lg";
 };
 
+const baseClasses =
+  "font-semibold focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:hover:cursor-not-allowed disabled:bg-gray-300 disabled:text-gray-400 active:translate-y-px";
+
+const toneClasses: Record<
+  NonNullable<ButtonProps["tone"]>,
+  Record<NonNullable<ButtonProps["impact"]>, string>
+> = {
+  default: {
+    bold: "bg-sky-500 text-white hover:bg-sky-600 focus-visible:ring-sky-500",
+    light:
+      "bg-sky-200 text-sky-900 hover:bg-sky-300 focus-visible:ring-sky-500",
+    bordered:
+      "outline outline-2 outline-sky-500 hover:bg-sky-200 focus-visible:ring-sky-500",
+  },
+  danger: {
+    bold: "bg-rose-400 hover:bg-rose-500 focus-visible:ring-rose-500",
+    light:
+      "bg-rose-200 text-rose-900 hover:bg-rose-300 focus-visible:ring-rose-500",
+    bordered:
+      "outline outline-2 outline-rose-500 hover:bg-rose-200 focus-visible:ring-rose-500",
+  },
+  warning: {
+    bold: "bg-amber-400 hover:bg-amber-500 focus-visible:ring-amber-500",
+    light:
+      "bg-amber-200 text-amber-900 hover:bg-amber-300 focus-visible:ring-amber-500",
+    bordered:
+      "outline outline-2 outline-amber-500 hover:bg-amber-200 focus-visible:ring-amber-500",
+  },
+  success: {
+    bold: "bg-green-400 hover:bg-green-500 focus-visible:ring-green-500",
+    light:
+      "bg-green-200 text-green-900 hover:bg-green-300 focus-visible:ring-green-500",
+    bordered:
+      "outline outline-2 outline-green-500 hover:bg-green-200 focus-visible:ring-green-500",
+  },
+};
+
+const shapeClasses: Record<NonNullable<ButtonProps["shape"]>, string> = {
+  square: "rounded-none",
+  rounded: "rounded",
+  pill: "rounded-full",
+};
+
 const sizeClasses: Record<NonNullable<ButtonProps["size"]>, string> = {
   sm: "p-2 text-sm",
   md: "px-4 py-3 text-base",
@@ -23,8 +66,10 @@ export const Button = ({
   return (
     <button
       className={twMerge(
-        "bg-sky-500 px-4 py-3 text-base font-semibold text-white hover:bg-sky-600 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-sky-500 focus-visible:ring-offset-2 active:translate-y-px disabled:bg-gray-300 disabled:text-gray-400 disabled:hover:cursor-not-allowed",
+        baseClasses,
+        shapeClasses[shape],
         sizeClasses[size],
+        toneClasses[tone][impact],
       )}
       {...restProps}
     />
